70cf41708fd61f01d5b644be104b2f4567080ed7
svarz <svarz@terpmail.umd.edu>
2022-04-15 17:54:26 -0400

    Initial Docstrings to visualization.py
    We will use this for implementing seaborn to create pie charts for comparing 2 different dataframes representing 2 cities, states, or countries depending on choice.

--- visualization.py
+++ visualization.py
@@ -1,4 +1,14 @@
-import seaborn as sns
+import seaborn as sns #Unsure if we need to import matplotlib as well?
 import dataAnalysis
 
-# this file will be for seaborn implementation
+
+
+
+# This file will be for seaborn implementation. We will create different plots to create visualizations based on the data that dataAnalysis.py generates.
+# Plan: import the 2 dataframe object variables to compare from the dataAnalysis.py file (from file.py import variableName), and create two pie charts
+# to compare genre listening habits by city/state/country.
+
+
+
+# Graph types to consider: pie chart, bar graph, or box plot? Pie chart seems most effective.
+# link for seaborn pie chart tutorial: https://www.statology.org/seaborn-pie-chart/
