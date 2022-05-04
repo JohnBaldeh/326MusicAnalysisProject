e4d560d7317bb8ecf4e3798dbf9d6bc95f926383
svarz <svarz@terpmail.umd.edu>
2022-05-04 14:54:59 -0400

    Added testing df to visualization.py

--- client/visualization.py
+++ client/visualization.py
@@ -1,8 +1,12 @@
 import seaborn as sns #Unsure if we need to import matplotlib as well?
-import client.dataAnalysis as dataAnalysis
+import dataAnalysis
+import pandas as pd
 
+# testing df:
+data = [['artist1', 'song1', 'Country'], ['artist2', 'song2', 'Hip-Hop Rap'], ['artist3', 'song3', 'Pop'], ['artist4', 'song4', 'Dance'], ['artist5', 'song5', 'Hip-Hop Rap'], ['artist6', 'song6', 'Country']]
+testing_df = pd.DataFrame(data, columns = ['Artist', 'Title', 'Genre'])
 
-
+# print(testing_df)
 
 # This file will be for seaborn implementation. We will create different plots to create visualizations based on the data that dataAnalysis.py generates.
 # Plan: import the 2 dataframe object variables to compare from the dataAnalysis.py file (from file.py import variableName), and create two pie charts
