9f894f15dea60d7e04082f632ea65905e6d36e26
svarz <svarz@terpmail.umd.edu>
2022-05-09 15:45:18 -0400

    Edited input data on visualization.py

--- client/dataAnalysis.py
+++ client/dataAnalysis.py
@@ -1,47 +1,77 @@
 import pandas as pd
+import os
 
-def genreCheck(df_origin, df_country, df_pop, df_hipHopRap, df_dance) :
+working_dir = os.path.dirname(__file__)
 
-	for row in df_origin.iterrows() :
-		if (df_origin.loc[row, {"Artist", "Title"}].isin(df_country)) :
-			# add 'Country' to df column: Genre. delete pass when implemented
-			df_origin.loc[row, "Genre"] = 'Country'
+# read csv files with all 200 songs for each country and store in array
+canada_songs = pd.read_csv(os.path.join(working_dir, "database/Canada/Shazam Top 100 Dance 25-04-2022.csv"))
+mexico_songs = pd.read_csv(os.path.join(working_dir, "database/Mexico/Shazam Top 200 Mexico Chart 25-04-2022.csv"))
+united_states_songs = pd.read_csv(os.path.join(working_dir, "database/United_States/Shazam_Top_200_United_States_Chart_25-04-2022.csv"))
+countries = [canada_songs, mexico_songs, united_states_songs]
 
-		elif (df_origin.loc[row, {"Artist", "Title"}].isin(df_pop)) :
-			# add 'Pop' to df column: Genre. delete pass when implemented
-			df_origin.loc[row, "Genre"] = 'Pop'
+# list all genres in each country with the pathname to the csv files with their songs
+canada_genres = {
+    "Dance": os.path.join(working_dir, "database/Canada/Shazam Top 100 Dance 25-04-2022.csv"),
+    "Hip-Hop Rap": os.path.join(working_dir, "database/Canada/Shazam Top 100 Hip-Hop_Rap 25-04-2022.csv"),
+    "Pop": os.path.join(working_dir, "database/Canada/Shazam Top 100 Pop 25-04-2022.csv")
+}
 
-		elif (df_origin.loc[row, {"Artist", "Title"}].isin(df_hipHopRap)) :
-			# add 'Hip-Hop Rap' to df column: Genre. delete pass when implemented
-			df_origin.loc[row, "Genre"] = 'Hip-Hop Rap'
+mexico_genres = {
+    "Dance": os.path.join(working_dir, "database/Mexico/Shazam Top 100 Dance 28-04-2022.csv"),
+    "Hip-Hop Rap": os.path.join(working_dir, "database/Mexico/Shazam Top 100 Hip-Hop_Rap 28-04-2022.csv"),
+    "Pop": os.path.join(working_dir, "database/Mexico/Shazam Top 100 Pop 28-04-2022.csv")
+}
 
-		elif(df_origin.loc[row, {"Artist", "Title"}].isin(df_dance)) :
-			# add 'Dance' to df column: Genre. delete pass when implemented
-			df_origin.loc[row, "Genre"] = 'Dance'
+united_states = {
+    "Dance": os.path.join(working_dir, "database/United_States/Shazam_Top_100_Dance_25-04-2022.csv"),
+    "Hip-Hop Rap": os.path.join(working_dir, "database/United_States/Shazam_Top_100_Hip-Hop_Rap_25-04-2022.csv"),
+    "Pop": os.path.join(working_dir, "database/United_States/Shazam_Top_100_Pop_25-04-2022.csv"),
+    "Country": os.path.join(working_dir, "database/United_States/Shazam_Top_100_Country_25-04-2022.csv"),
+}
 
-		else :
-			pass
+# store all country genres in array
+country_genres = [canada_genres, mexico_genres, united_states]
 
+# store country name in array to find sub-folders to store results later
+country_name = ["Canada", "Mexico", "United_States"]
 
-	# change to return, or have this passed to visualization.py later.
-	print(df_origin)
+# start iteration at Canada (index 0)
+index = 0
+for country in countries:
+    # get only Artist and Title columns
+    main_extract = country.loc[:, ["Artist", "Title"]]
 
+    # get list of genres of that country from the genres array
+    genres = country_genres[index]
 
+    # new array with second dimension of "Artists", "Title" , "Genre" to store values
+    new_array = [[], [], []]
 
-# FOR TESTING. REPLACE FUNCTIONALITY WITH if __name__ == "__main__" FUNCTION
+    # iterate through all songs in "200 songs" csv file
+    for song in main_extract.values:
 
-def main() :
-	df_origin = pd.read_csv('database/United_States/Shazam_Top_200_United_States_Chart_25-04-2022.csv')
+        # iterate through all csv files in each genre
+        for genre_name, file_name in genres.items():
 
-	df_country = pd.read_csv('database/United_States/Shazam_Top_100_Country_25-04-2022.csv')
-	df_pop = pd.read_csv('database/United_States/Shazam_Top_100_Pop_25-04-2022.csv')
-	df_hipHopRap = pd.read_csv('database/United_States/Shazam_Top_100_Hip-Hop_Rap_25-04-2022.csv')
-	df_dance = pd.read_csv('database/United_States/Shazam_Top_100_Dance_25-04-2022.csv')
+            # extract only artist and title
+            genre_extract = pd.read_csv(file_name).loc[:, ["Artist", "Title"]]
 
-	# print(df_origin)
+            # iterate through each song and compare with main csv file, if they match, append to 2d-array
+            for x in genre_extract.values:
+                if (song == x).all():
+                    new_array[0].append(x[0])
+                    new_array[1].append(x[1])
+                    new_array[2].append(genre_name)
 
-	# print(df_origin.loc[:, {"Artist", "Title"}])
+    # create data frame from 2d-array
+    final_data_frame = pd.DataFrame({
+        "Artist": new_array[0],
+        "Title": new_array[1],
+        "Genre": new_array[2]
+    })
 
-	genreCheck(df_origin, df_country, df_pop, df_hipHopRap, df_dance)
+    # save data frame to csv file, directory depends on country name
+    final_data_frame.to_csv(os.path.join(working_dir, f"database/{country_name[index]}/{country_name[index]}Results.csv"))
 
-main()
+    # increment the index to go to the next country
+    index += 1
