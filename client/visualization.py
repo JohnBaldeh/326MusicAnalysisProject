a3ee6c938213f61919c8651a495428034544447d
JohnBaldehDskTop <104944118+JohnBaldehDskTop@users.noreply.github.com>
2022-05-13 19:13:15 +0300

    Update visualization.py
    Made pie plots and save charts as png

--- client/visualization.py
+++ client/visualization.py
@@ -2,8 +2,8 @@
 
 
 from re import L
-import matplotlib.pyplot as plt 
-import seaborn as sns
+import matplotlib.pyplot as plt
+from pyparsing import col
 from dataAnalysis import *
 import pandas as pd
 
@@ -26,52 +26,68 @@ working_dir = os.path.dirname(__file__)
 # mexico = plt.pie(mexico_data, labels = labels, colors = palette_color, autopct='%.0f%%')
 # united_states = plt.pie(us_data, labels = labels, colors = palette_color, autopct='%.0f%%')
 
-class PiePlot :
 
-	def __init__(self, countryName, colName, figSize = (5,5)) :
-		""" Initializes the attributes of a plot.
-		"""
-		
-		countryName = self.countryName
-		colName = self.colName
-		figSize = self.figSize
+class PiePlot:
 
-	def instanceCount(countryObject) :
-		""" Tracks the instances of each genre within the genre column (allows for "easier" pie plot creation, since it wasn't working with strings)
-		"""
-	# canada_occurences = canada_results['Genre'].value_counts()
-	# mexico_occurences = mexico_results['Genre'].value_counts()
-	# us_occurences = united_states_results['Genre'].value_counts()
-		instances = ((working_dir, f"database/{countryObject.country_name[index]}/countryObject.{country_name[index]}Results.csv")[countryObject.colName].value_counts())
-		return instances[1]
+    def __init__(self, instance_data, countryName, colName, figSize=(5, 5)):
+        """ Initializes the attributes of a plot.
+        """
+        self.country_name = countryName
+        self.colName = colName
+        self.figsize = figSize
+        self.instance_data = instance_data
 
-	def makePlot(instances, figSize) :
-		""" Creates a pie plot with the integer instances of each genre appearing in the top 200 songs. DOES NOT WORK YET***
-		"""
+    def instanceCount(self):
+        """ Tracks the instances of each genre within the genre column (allows for "easier" pie plot creation, since it wasn't working with strings)
+        """
+    # canada_occurences = canada_results['Genre'].value_counts()
+    # mexico_occurences = mexico_results['Genre'].value_counts()
+    # us_occurences = united_states_results['Genre'].value_counts()
+        instances = (working_dir, f"/database/{self.country_name}/{self.country_name}Results.csv")
+        colName = self.colName
+        allvalues = getattr(pd.read_csv("".join(instances)), colName)
+        counts = allvalues.value_counts()
+                    
+        #return series of all the genre counts
+        return counts
 
-		plot = instances.plot.pie(y='Genre')
-		print(plot)
+    def makePlot(instances, figSize):
+        """ Creates a pie plot with the integer instances of each genre appearing in the top 200 songs.
+        """
+
+        plot = instances.plot.pie(y='Genre', subplots = True)
+
+        return plot
 
 
-def main(colName) :
-	""" "Driver" for the program. Creates the plot objects by calling the PiePlot init, then uses the methods within PiePlot to further refine the dataframes into a pie plot.
-	"""
-	canada_results = pd.read_csv(os.path.join(working_dir, "database/Canada/CanadaResults.csv"))
-	mexico_results = pd.read_csv(os.path.join(working_dir, "database/Mexico/MexicoResults.csv"))
-	united_states_results = pd.read_csv(os.path.join(working_dir, "database/United_States/United_StatesResults.csv"))
+def main(colName):
+    """ "Driver" for the program. Creates the plot objects by calling the PiePlot init, then uses the methods within PiePlot to further refine the dataframes into a pie plot.
+    """
+    canada_results = pd.read_csv(os.path.join(
+        working_dir, "database/Canada/CanadaResults.csv"))
+    mexico_results = pd.read_csv(os.path.join(
+        working_dir, "database/Mexico/MexicoResults.csv"))
+    united_states_results = pd.read_csv(os.path.join(
+        working_dir, "database/United_States/United_StatesResults.csv"))
 
-	canada_instances = PiePlot(canada_results, country_name[0], 'Genre', (5, 5))
-	mexico_instances = PiePlot(mexico_results, country_name[1], 'Genre', (5, 5))
-	united_states_instances = PiePlot(united_states_results, country_name[2], 'Genre', (5, 5))
+    canada_instances = PiePlot(
+        canada_results, country_name[0], 'Genre', (5, 5))
+    mexico_instances = PiePlot(
+        mexico_results, country_name[1], 'Genre', (5, 5))
+    united_states_instances = PiePlot(
+        united_states_results, country_name[2], 'Genre', (5, 5))
 
-	canada_count = PiePlot.instanceCount(canada_instances)
-	mexico_count = PiePlot.instanceCount(mexico_instances)
-	united_states_count = PiePlot.instanceCount(united_states_instances)
+    canada_count = canada_instances.instanceCount()
+    mexico_count = mexico_instances.instanceCount()
+    united_states_count = united_states_instances.instanceCount()
 
-	#save these as pngs, or get them to display (maybe better to display using py notebook?)
-	canadaPlot = PiePlot.makePlot(canada_count, (5, 5))
-	mexicoPlot = PiePlot.makePlot(mexico_count, (5, 5))
-	unitedStatesPlot = PiePlot.makePlot(united_states_count, (5, 5))
+    # save these as pngs, or get them to display (maybe better to display using py notebook?)
+    canadaPlot = PiePlot.makePlot(canada_count, (5, 5))    
+    plt.savefig("canadaGenresPlot.png")
+    mexicoPlot = PiePlot.makePlot(mexico_count, (5, 5))
+    plt.savefig("MexicoGenresPlot.png") 
+    unitedStatesPlot = PiePlot.makePlot(united_states_count, (5, 5))
+    plt.savefig("unitedStatesGenresPlot.png")    
 
 
 main('Genre')
